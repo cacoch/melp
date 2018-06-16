2e0ab3a9690c9273d449da8d26dc53e5a23a7c0a
zdenek <zdenek@venders.mx>
2018-06-16 11:45:41 -0500

    gulp with restaurant example

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,28 @@ var reload = browserSync.reload;
  *  * Compile pug files into HTML
  *   */
 gulp.task('templates', function() {
-    var YOUR_LOCALS = {
-        message: 'This app is powered by gulp.pug recipe for BrowserSync'
-    };
+    var RESTAURANT={
+			"id":"851f799f-0852-439e-b9b2-df92c43e7672",
+			"rating":1,
+			"name":"Barajas, Bahena and Kano",
+			"contact":{
+					"site":"https://federico.com",
+					"email":"Anita_Mata71@hotmail.com",
+					"phone":"534 814 204"},
+			"address":{
+					"street":"82247 Mariano Entrada",
+					"city":"Mérida Alfredotown",
+					"state":"Durango",
+			"location":{
+					"lat":19.440057053713137,
+					"lng":-99.12704709742486
+			}}};
 
     return gulp
         .src('./src/*.pug')
         .pipe(
             pug({
-                locals: YOUR_LOCALS
+                locals: RESTAURANT 
             })
         )
         .pipe(gulp.dest('./dist/'));
